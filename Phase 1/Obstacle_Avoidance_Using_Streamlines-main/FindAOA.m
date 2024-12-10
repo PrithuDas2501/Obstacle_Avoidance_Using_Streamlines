602879b5ee6b4dfd04d8635cc03c1db19d4a9045
Hrishav Das <72787333+PrithuDas2501@users.noreply.github.com>
2024-12-10 18:39:48 +0530

    Delete Phase 1/Obstacle_Avoidance_Using_Streamlines-main directory

--- Phase 1/Obstacle_Avoidance_Using_Streamlines-main/FindAOA.m
+++
@@ -1,6 +0,0 @@
-function y = FindAOA(theta, Rx, C, Xdash, Zdash, A)
-%% Needs to account for non centre aligned velocity!!!!!
-%% Actual Angle of attack will be this angle found minus angle between
-Rotation_Matrix = RotMatrix(theta,Zdash);
-y = ((C+Rx*Rotation_Matrix*Xdash)'*A*(C+Rx*Rotation_Matrix*Xdash)-1)^2;
-end
--- Phase 1/Obstacle_Avoidance_Using_Streamlines-main/find_stream.m
+++
@@ -1,9 +0,0 @@
-function y = find_stream(r,alpha,ALPHA,rx,ry)
-X = -r*cos(ALPHA);
-Y = r*sin(ALPHA);
-
-A = Y*cos(alpha)/ry + X*sin(alpha)/rx;
-B = 1/((X*cos(alpha)/rx - Y*sin(alpha)/ry)^2 + (Y*cos(alpha)/ry + X*sin(alpha)/rx)^2);
-
-y = A*(1-B);
-end
